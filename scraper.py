c826d3bef7e631a8efca1637554e03cf80c46469
Lazar Telebak <lazartelebak@gmail.com>
2017-05-23 20:06:41 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,45 +1,26 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-from selenium import webdriver
 import scraperwiki
 from time import sleep
 from splinter import Browser
-
-import platform
-print platform.platform()
-
-print 'fooobar'
-
-# Read in a page
-html = scraperwiki.scrape("http://foo.com")
+from selenium import webdriver
 
 
-# Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-
-with Browser("phantomjs") as browser:
+with Browser('phantomjs') as browser:
     # Visit URL
-    url = "http://www.google.com"
+    url = "https://www.google.rs/search?q=bitcoin&tbm=nws&cad=h"
     browser.visit(url)
-    browser.fill('q', 'splinter - python acceptance testing for web applications')
-    # Find and click the 'search' button
-    button = browser.find_by_name('btnG')
-    # Interact with elements
-    button.click()
-    if browser.is_text_present('splinter.readthedocs.io'):
-        print("Yes, the official website was found!")
-    else:
-        print("No, it wasn't found... We need to improve our SEO techniques")
+    sleep(2)
 
-scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "some name", "occupation": "software"})
+    titles = browser.find_by_xpath('//*[@class="l _PMs"]')
+    times = browser.find_by_xpath('//*[@class="f nsa _QHs"]')
+    urls = browser.find_by_xpath('//*[@class="top _xGs _SHs"]')
+    for title, time, url in zip(titles, times, urls):
+        print '-' * 60
+        print title.text
+        print time.text
+        print url['href']
+        print '-' * 60
+        print '\n'
 
-# An arbitrary query against the database
-scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+        scraperwiki.sqlite.save(data={'title': title.text,
+                                      'time': time.text,
+                                      'url': url['href']})
