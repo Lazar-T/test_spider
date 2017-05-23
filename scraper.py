c6e3eaf6a5168169a7a6573d797bf3b38422a44f
Lazar Telebak <lazartelebak@gmail.com>
2017-05-23 15:21:48 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,21 +1,25 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import selenium
+import scraperwiki
+import lxml.html
+
+# Read in a page
+html = scraperwiki.scrape("http://foo.com")
+
+# Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+root.cssselect("div[align='left']")
+
+driver = selenium.webdriver.Firefox()
+driver.get('https://www.reddit.com/')
+title = driver.title
+
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": title, "occupation": "software developer"})
+
+# An arbitrary query against the database
+scraperwiki.sql.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
